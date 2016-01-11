317beb9ddda988506990efd4fee8fc09a2d02e3c
Steven Jay <stevenjasionowicz@gmail.com>
2016-01-11 23:04:26 +1300

    Set up Routing

--- app/core/App.php
+++ app/core/App.php
@@ -2,8 +2,22 @@
 
 class App
 {
+
+	protected $controller = 'home';
+
+	protected $method = 'index';
+
+	protected $params = [];
+
 	public function __construct()
 	{
-		echo 'Hello';
+		$url = $this->parseUrl();
+	}
+
+	public function parseUrl()
+	{
+		if(isset($_GET['url'])) {
+			return $url = explode('/',filter_var(rtrim($_GET['url'], '/'), FILTER_SANITIZE_URL));
+		}
 	}
 }
